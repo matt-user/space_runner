ea004cfd38b688ae7c0e0e0837bb20ae57d04c8b
Matt Auer <mattauer@umich.edu>
2020-04-23 21:45:23 -0400

    add global variables for where the screen pops up

--- config.py
+++ config.py
@@ -1,4 +1,9 @@
-# global variables that are shared across files
+# global variables that are shared across files]
+# i hope this is how I do it lol
+
+# constants for game window position
+X_WIN_POS = 50
+Y_WIN_POS = 50
 
 # constants for screen width and height
 SCREEN_WIDTH = 600
