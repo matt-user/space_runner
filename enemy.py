251ceb82c0a10924dc378f1865d17204d17a02ca
Matt Auer <mattauer@umich.edu>
2020-04-24 14:51:50 -0400

    added animation to the floater enemy

--- enemy.py
+++ enemy.py
@@ -2,15 +2,14 @@
 
 import pygame
 import random
+
 import config
+from animation import Animation
 
 # Enemy class that tries to destroy user
 class Enemy(pygame.sprite.Sprite):
-	def __init__(self, center):
+	def __init__(self):
 		super().__init__()
-		self.surf = pygame.Surface((20, 10))
-		self.surf.fill((255, 255, 255))
-		self.rect = self.surf.get_rect(center=center)
 
 	# remove the sprite when it passes the bottom edge of the screen
 	def update(self):
@@ -24,12 +23,19 @@ class Floater(Enemy):
 	# Minimum and maximum possible speeds for this enemy
 	MIN_SPEED_POSSIBLE = 3
 	MAX_SPEED_POSSIBLE = 5
+	ANIMATION_DELAY = 10
+
 	def __init__(self, center):
-		super().__init__(center)
+		super().__init__()
+		# load all of the floater's animations
+		self.animation = Animation(Floater.ANIMATION_DELAY, 'assets', 'enemy')
+		self.surf = self.animation.get_image()
+		self.rect = self.surf.get_rect(center=center)
 		self.speed = random.randint(Floater.MIN_SPEED_POSSIBLE, Floater.MAX_SPEED_POSSIBLE)
 	
 	def update(self):
 		"""Update the behavior of a Floater enemy."""
+		self.surf = self.animation.next_animation()
 		self.rect.move_ip(0, self.speed)
 		Enemy.update(self)
 	
