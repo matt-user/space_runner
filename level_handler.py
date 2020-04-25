d8f784d60e8edda1b6f1ab118141794f80b43ccc
Matt Auer <mattauer@umich.edu>
2020-04-25 17:35:31 -0400

    changed temporarily for testing

--- level_handler.py
+++ level_handler.py
@@ -4,20 +4,23 @@ import pygame
 
 import config
 from enemy import Floater
+from enemy import Mall_Fighter
 from model import Model
 
 class Level_Handler():
     def __init__(self):
         """Construct level handler."""
+        """self.level_one_enemy_locations = [
+            (10, -10), (50, -50), (config.SCREEN_WIDTH / 2, -900),
+            ((config.SCREEN_WIDTH / 2) - 30, -1000), ((config.SCREEN_WIDTH / 2) + 50, -1100)
+        ]"""
         self.level_one_enemy_locations = [
-            (5, -10), (10, -10), (config.SCREEN_WIDTH / 2, -15),
-            ((config.SCREEN_WIDTH / 2) + 3, -18), (config.SCREEN_WIDTH - 20, -15),
-            (config.SCREEN_WIDTH - 40, -17), (config.SCREEN_WIDTH - 50, -10)
+            (config.SCREEN_WIDTH / 2, 0)
         ]
 
     def load_level(self, level):
         """Load a given level based on the level number"""
         if level == 1:
             for location in self.level_one_enemy_locations:
-                new_enemy = Floater(location)
+                new_enemy = Mall_Fighter(location)
                 Model.get_instance().add_enemy(new_enemy)
