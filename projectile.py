1932eab8db0257997485ddd53089965d361b2899
Matt Auer <mattauer@umich.edu>
2020-04-28 14:07:06 -0400

    added rotating functionality so bullet top heads in given direction

--- projectile.py
+++ projectile.py
@@ -1,4 +1,5 @@
 import os
+import math
 import pygame
 
 import config
@@ -13,14 +14,16 @@ class Projectile(pygame.sprite.Sprite):
 
 	def __init__(self, center, direction=(0, -1)):
 		super().__init__()
-		# load all of the projectile animations
-		self.animation = Animation(Projectile.ANIMATION_DELAY, 'assets', 'projectile')
-		self.surf = self.animation.get_image()
-		self.rect = self.surf.get_rect(center=center)
 		self.speed = 6
 		self.x_dir, self.y_dir = direction
 		self.x_dir *= self.speed
 		self.y_dir *= self.speed
+		# load all of the projectile animations
+		rotation_angle = self.get_rotation_angle()
+		self.animation = Animation(Projectile.ANIMATION_DELAY, 'assets', 'projectile')
+		self.animation.rotate_center(rotation_angle)
+		self.surf = self.animation.get_image()
+		self.rect = self.surf.get_rect(center=center)
 	
 	def update(self):
 		"""update the projectile's location"""
@@ -34,3 +37,11 @@ class Projectile(pygame.sprite.Sprite):
 		if (self.rect.bottom < 0 or self.rect.top > config.SCREEN_HEIGHT
 			or self.rect.right < 0 or self.rect.left > config.SCREEN_WIDTH):
 			self.kill()
+	
+	def get_rotation_angle(self):
+		"""Returns the rotation angle of the projectile."""
+		rotation_angle = math.atan(self.x_dir / self.y_dir) * (180.0 / math.pi)
+		# flip rotation if y_dir is positive
+		if self.y_dir > 0:
+			rotation_angle += 180
+		return rotation_angle
