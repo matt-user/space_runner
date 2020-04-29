f306f27ded0e7cb70d6f59c88222729ba7cf5e30
Matt Auer <mattauer@umich.edu>
2020-04-29 15:03:41 -0400

    moved get direction function from enemy to utility, probably should go in a geometry class

--- utility.py
+++ utility.py
@@ -1,4 +1,7 @@
+import operator
+import math
 import pygame
+
 import config
 
 # Functions needed from multiple files
@@ -11,3 +14,15 @@ def get_image(path):
 	else:
 		image = config._image_library[path]
 	return image
+
+# TODO: make a geometry class for shit like this
+def get_direction(point_a, point_b):
+	"""Returns the normalized direction from point a to the point b."""
+	x_dif, y_dif = map(operator.sub, point_b, point_a)
+	magnitude = math.sqrt((x_dif ** 2) + (y_dif ** 2))
+	# if the points are the same return a 0 vector
+	if magnitude == 0:
+		return 0, 0
+	x_dif_normed = x_dif / magnitude
+	y_dif_normed = y_dif / magnitude
+	return x_dif_normed, y_dif_normed
