965beb31f71a1134f7616b209d633f5b5cf2521d
Matt Auer <mattauer@umich.edu>
2020-05-01 18:05:54 -0400

    changed way moving object handles the objects position

--- moving_object.py
+++ moving_object.py
@@ -2,6 +2,8 @@
 
 import operator
 
+from pygame.math import Vector2
+
 from utility import get_direction
 
 class MovingObjectMixin:
@@ -12,20 +14,22 @@ class MovingObjectMixin:
         self.is_moving = False
         self.delta = (0, 0)
 
-    def start_moving(self, point):
+    def start_moving(self, destination):
         """Tells the moving object to start moving to the point."""
-        if self.get_pos() == point:
+        if self.get_pos() == destination:
             if self.is_moving:
                 self.stop_moving()
             return
         self.is_moving = True
-        self.destination = point
+        self.destination = destination
         self.compute_delta()
 
     def compute_delta(self):
         """Compute the change in the x and y direction for each update"""
-        x_dir, y_dir = get_direction(self.get_pos(), self.destination)
-        self.delta = (x_dir * self.speed, y_dir * self.speed)
+        direction = self.destination - self.get_pos()
+        # x_dir, y_dir = get_direction(self.get_pos(), self.destination)
+        direction.normalize_ip()
+        self.delta = (direction.x * self.speed, direction.y * self.speed)
 
     def update_location(self):
         """Update the location of the moving object."""
@@ -35,23 +39,23 @@ class MovingObjectMixin:
             self.set_pos(self.destination)
             self.stop_moving()
             return True
-        x_dir, y_dir = get_direction(self.get_pos(), self.destination)
-        self.rect.move_ip(self.speed * x_dir, self.speed * y_dir)
+        self.rect.move_ip(self.delta[0], self.delta[1])
+        # x_dir, y_dir = get_direction(self.get_pos(), self.destination)
+        # self.rect.move_ip(self.speed * x_dir, self.speed * y_dir)
         return False
 
     def is_at_location(self, point):
         """Returns whether the enemy is at the given point."""
-        x_dif, y_dif = map(operator.sub, point, self.get_pos())
-        return (abs(x_dif) <= abs(self.delta[0])) and (abs(y_dif) <= abs(self.delta[1]))
-
-    def get_pos(self):
-        """Returns the middle front of an enemy"""
-        x, y = self.rect.midtop
-        return x, y
+        dif = self.destination - self.get_pos()
+        print(f"dets: {self.destination} pos: {self.get_pos()}")
+        print(f"dif: {dif} delta: {self.delta}")
+        # x_dif, y_dif = map(operator.sub, point, self.get_pos())
+        return (abs(dif.x) <= abs(self.delta[0])) and (abs(dif.y) <= abs(self.delta[1]))
     
     def set_pos(self, point):
         """Sets the position of the moving object"""
-        self.rect.midtop = point
+        self.gun_pos = point
+        self.rect.center = self.gun_pos - ((self.original_surf.get_height() / 2) * self.direction)
 
     def stop_moving(self):
         """Tells this object to stop moving"""
