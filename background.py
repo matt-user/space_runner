1b0814b7b1c19f5603c64b36b2cbf53f82394ed6
Matt Auer <mattauer@umich.edu>
2020-05-04 13:54:38 -0400

    added background class

---
+++ background.py
@@ -0,0 +1,43 @@
+"""Class for maintaining the background image."""
+
+import os
+import pygame
+
+from utility import get_image
+
+class Background():
+    def __init__(self):
+        self.color = pygame.Color("black")
+    
+    def set_tiles(self, screen):
+        """Load the images for the background tiles."""
+        # load the background tiles
+        image_folder_path = os.path.join('assets', 'background')
+        image_files = [os.path.join(image_folder_path, f) for f in os.listdir(image_folder_path)]
+        image_files = [
+                        [image_files[0], image_files[1]],
+                        [image_files[2], image_files[3]]
+                    ]
+        self.tiles = [[get_image(f) for f in row] for row in image_files]
+        self.stage_pos_x = 0
+        self.stage_pos_y = 0
+        self.tile_width = self.tiles[0][0].get_width()
+        self.tile_height = self.tiles[0][0].get_height()
+        screen.blit(self.tiles[0][0], [0, 0])
+        self.surface = screen.copy()
+
+    def scroll(self, x, y, screen):
+        """Scroll the background."""
+        self.stage_pos_x -= x
+        self.stage_pos_y -= y
+        col = (self.stage_pos_x % (self.tile_width * len(self.tiles[0]))) // self.tile_width
+        x_off = (0 - self.stage_pos_x % self.tile_width)
+        row = (self.stage_pos_y % (self.tile_height * len(self.tiles))) // self.tile_height
+        y_off = (0 - self.stage_pos_y % self.tile_height)
+        col2 = ((self.stage_pos_x + self.tile_width) % (self.tile_width * len(self.tiles[0]))) // self.tile_width
+        row2 = ((self.stage_pos_y + self.tile_height) % (self.tile_height * len(self.tiles))) // self.tile_height
+        screen.blit(self.tiles[row][col], [x_off, y_off])
+        screen.blit(self.tiles[row][col2], [x_off + self.tile_width, y_off])
+        screen.blit(self.tiles[row2][col], [x_off, y_off + self.tile_height])
+        screen.blit(self.tiles[row2][col2], [x_off + self.tile_width, y_off + self.tile_height])
+        self.surface = screen.copy()
