5781f5940f1f240675bc95207dd15319025654d2
Matt Auer <mattauer@umich.edu>
2020-05-04 13:51:55 -0400

    changed the screen width and height constants

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ X_WIN_POS = 50
 Y_WIN_POS = 50
 
 # constants for screen width and height
-SCREEN_WIDTH = 600
-SCREEN_HEIGHT = 700
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 720
 
 # global image dictionary for loading images
 _image_library = {}
